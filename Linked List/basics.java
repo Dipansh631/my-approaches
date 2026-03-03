36f0a89e1408ed4f77c57f265f40664506770fba
Dipanshu Maheshwari <dipanshu.maheshwari_cs.aiml24@gla.ac.in>
2026-03-03 21:18:43 +0530

    Implement basic linked list structure in Java

---
+++ Linked List/basics.java
@@ -0,0 +1,25 @@
+package LinkedList;
+
+public class Basics {
+    public static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data=data;
+        }
+    }
+    public static void main(String[]args){
+        Node a=new Node(1);
+        Node b=new Node(2);
+        Node c=new Node(3);
+        Node d=new Node(4);
+        Node e=new Node(5);
+        a.next=b;
+        b.next=c;
+        c.next=d;
+        d.next=e;
+        e.next=a;
+        System.out.println(a.data);
+        System.out.println(a.next.data);
+    }
+}
