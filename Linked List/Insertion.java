453af0cd70fe2cde19007b6b7ae0966faae35b97
Dipanshu Maheshwari <dipanshu.maheshwari_cs.aiml24@gla.ac.in>
2026-03-03 21:26:16 +0530

    Add linked list implementation with node insertion
    Implemented a basic linked list with node insertion.

---
+++ Linked List/Insertion.java
@@ -0,0 +1,28 @@
+package LinkedList;
+
+public class Basics {
+    public static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data=data;
+        }
+    }
+    public static void main(String[]args){
+        Node a=new Node(1);
+        Node b=new Node(2);
+        Node c=new Node(3);
+        Node d=new Node(4);
+        Node e=new Node(5);
+        Node f=new Node(9);
+        a.next=b;
+        b.next=c;
+        c.next=d;
+        d.next=e;
+        e.next=a;
+        c.next=f; //Insertion in middle
+        f.next=d;
+        System.out.println(f.data);
+        System.out.println(c.next.data);
+    }
+}
