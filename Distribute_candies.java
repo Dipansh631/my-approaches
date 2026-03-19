4cd1083054d86100362298398103d71b6189ab6d
Dipansh631 <dipanshu.maheshwari_cs.aiml24@gla.ac.in>
2026-03-19 11:45:17 +0530

    Remove root-level duplicate files and keep folder structure

--- Distribute_candies.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int distributeCandies(int[] candyType) {
-        HashSet<Integer> myset =new HashSet<>();
-        for(int var:candyType){
-            myset.add(var);
-        }
-        return Math.min(myset.size(),candyType.length/2);
-    }
-}
--- sqrt(x).java
+++
@@ -1,6 +0,0 @@
-class Solution {
-    public int mySqrt(int x) {
-        double k= Math.pow(x,0.5);
-        return (int)k;
-    }
-}
