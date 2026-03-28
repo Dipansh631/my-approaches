5a02b7dcbc64e234497a09e24c28aaf2bf0db9e2
Dipanshu Maheshwari <dipanshu.maheshwari_cs.aiml24@gla.ac.in>
2026-03-28 13:24:02 +0530

    Add functionality to insert elements at any index in stack
    Implemented a method to insert an element at a specified index in a stack.

---
+++ Stacks/Insert_elements_at_index.java
@@ -0,0 +1,26 @@
+package Stacks;
+
+import java.util.Stack;
+
+public class Insert_at_Bottom_or_anyIndex {
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+        st.push(1);
+        st.push(2);
+        st.push(3);
+        st.push(4);
+        st.push(5);
+        System.out.println(st);
+        int idx=2;
+        int x=10;
+        Stack<Integer> st1 = new Stack<>();
+        while(st.size()>idx){
+            st1.push(st.pop());
+        }
+        st.push(x);
+        while(!st1.isEmpty()){
+            st.push(st1.pop());
+        }
+        System.out.println(st);
+    }
+}
