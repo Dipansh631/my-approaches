a51b20ce8df9a7f8625f7c9c45069eb97be3f573
Dipanshu Maheshwari <dipanshu.maheshwari_cs.aiml24@gla.ac.in>
2026-03-29 20:54:51 +0530

    Implement Reverse Polish Notation evaluator

---
+++ Stacks/Evaluate_reverse_polish_notation.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> st=new Stack <>();
+        String Operators="+-/*";
+        for(String token:tokens){
+            if(Operators.indexOf(token)!=-1){
+                int d1=st.pop();
+                int d2=st.pop();
+                int res=0;
+                if(token.equals("+")) res=d1+d2;
+                else if(token.equals("-")) res=d2-d1;
+                else if(token.equals("*")) res=d1*d2;
+                else if(token.equals("/")) res=d2/d1;
+                st.push(res);
+            }
+            else{
+                st.push(Integer.parseInt(token));
+            }
+        }
+        return st.peek();
+    }
+}
