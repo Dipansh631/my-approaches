449fe15ac634e9aab3917cb058843af48986e786
Dipanshu Maheshwari <dipanshu.maheshwari_cs.aiml24@gla.ac.in>
2026-04-24 23:50:01 +0530

    Implement method to check for perfect square

---
+++ try/367. Valid Perfect Square.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        if (num < 0) return false;
+        int root = (int) Math.sqrt(num);
+        return (root * root == num);
+    }
+}
