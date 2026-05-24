b1fda710b7ab467b4d57ac26d8bacd0b17101df7
Dipanshu Maheshwari <dipanshu.maheshwari_cs.aiml24@gla.ac.in>
2026-05-24 12:46:22 +0530

    Implement isUgly method to check ugly numbers

---
+++ try/263. ugly number.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isUgly(int n) {
+
+        if (n <= 0) return false;
+
+        while (n % 2 == 0) n /= 2;
+        while (n % 3 == 0) n /= 3;
+        while (n % 5 == 0) n /= 5;
+
+        return n == 1;
+    }
+}
+
+
+//////////////////////////////////////////////////////////////////////////OR//////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+---->>> // Time Limit Exceeds
+class Solution {
+    public boolean isUgly(int n) {
+        if(n<0)return false;
+        int[]factors={2,3,5};
+        for(int f:factors){
+            while(n%f==0){
+                n=n/f;
+            }
+        }
+        return n==1;
+    }
+}
