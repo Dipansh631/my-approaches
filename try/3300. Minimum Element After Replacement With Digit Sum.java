84bf46dbb992e383efd766b0577e9b9071a11e0f
Dipanshu Maheshwari <dipanshu.maheshwari_cs.aiml24@gla.ac.in>
2026-05-29 19:46:31 +0530

    Refactor minElement to use digit sum for minimum value
    Refactored minElement method to calculate minimum element based on digit sum.

---
+++ try/3300. Minimum Element After Replacement With Digit Sum.java
@@ -0,0 +1,30 @@
+///////////5ms
+class Solution {
+    public int minElement(int[] nums) {
+        for(int i=0;i<nums.length;i++){
+            int sum=0;
+            while(nums[i]>0){
+                sum+=nums[i]%10;
+                nums[i]/=10;
+            }
+            nums[i]=sum;
+        }
+        int min = Arrays.stream(nums).min().getAsInt();
+        return min;
+    }
+}
+///////////1ms
+class Solution {
+    public int minElement(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        for(int i=0;i<nums.length;i++){
+            int sum=0;
+            while(nums[i]>0){
+                sum+=nums[i]%10;
+                nums[i]/=10;
+            }
+            min = Math.min(sum, min);
+        }
+        return min;
+    }
+}
